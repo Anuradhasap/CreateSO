a7995ce6dba6cd1d2608f662636640020320270d
anu ch <anuradhach.usa@gmail.com>
2024-03-08 23:41:28 +0000

    createso

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("createso.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"createso/test/unit/controller/Screen1.controller"
+], function () {
+	"use strict";
+});
